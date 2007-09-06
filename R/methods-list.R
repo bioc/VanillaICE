4f8a55a7d68a23ed0c941f2ec4acac2bd9ff3e0a
Rob Scharp <rscharpf@jhsph.edu>
2007-09-06 15:23:25 +0000

    export unsplitHmm
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VanillaICE@26892 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-list.R
+++ R/methods-list.R
@@ -3,7 +3,8 @@ setMethod(".unsplitHmm", c("list", "AnnotatedDataFrame"),
             object <- switch(class(from[[1]]),
                              HmmSnpSet=new("HmmSnpSet", ...),
                              HmmSnpCallSet=new("HmmSnpCallSet", ...),
-                             HmmSnpSet=new("HmmSnpSet", ...))
+                             HmmSnpSet=new("HmmSnpSet", ...),
+                             stop("Must be a class defined in VanillaICE"))
             featureData(object) <- annotatedDataFrame[match(featureNames(object), featureNames(annotatedDataFrame)), , ]
             stopifnot(identical(rownames(copyNumber(object)), rownames(fData(object))))            
             object
